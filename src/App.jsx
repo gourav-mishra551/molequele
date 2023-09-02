1c6b7462cbd327e4365370e3368915d6f28c2ddb
gourav-mishra551 <gouravmishra200105@gmail.com>
2023-09-02 13:31:16 +0530

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import Contact from './pages/Contact/Contact';
 import Industry from './pages/Industries/Industries';
 import Home from './pages/Home/Home'
 import Products from "./pages/Products/Products"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/industry" element={<Industry />} />
         <Route exact path="/products" element={<Products />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </>
   )
 }
